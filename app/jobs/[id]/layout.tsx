c99320a368f29888749fc0ec14724a5e1052f497
iwasakitakehiro <iwasakitakehiro2@gmail.com>
2023-05-11 18:09:21 +0900

    初期設定

---
+++ app/jobs/[id]/layout.tsx
@@ -0,0 +1,7 @@
+export default function JobsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="m-4 font-bold">{children}</div>;
+}
---
+++ app/jobs/list/page.tsx
@@ -0,0 +1,9 @@
+const List = () => {
+  return (
+    <main>
+      <h2>Job List</h2>
+    </main>
+  );
+};
+
+export default List;
