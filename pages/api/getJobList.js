02e2e9cabf438a271b649a9c7174d08ecf8bc0c6
takehiro <iwasakitakehiro2@gmail.com>
2023-09-24 01:07:09 +0900

    デプロイ

--- pages/api/getJobList.js
+++ pages/api/getJobList.js
@@ -1,12 +1,11 @@
 import { prisma } from "@/lib/prisma";
 
-export default async function handler(req, res) {
-  try {
-    const page = Number(req.query.page) || 1; // クエリからページ番号を取得
+class JobService {
+  async fetchJobs(page) {
     const limit = 20; // 1ページあたりのレコード数
     const skip = (page - 1) * limit; // スキップするレコード数
 
-    const jobs = await prisma.job.findMany({
+    return prisma.job.findMany({
       skip: skip, // ページネーションのためのスキップ
       take: limit, // 1ページあたりのレコード数
       include: {
@@ -16,9 +15,19 @@ export default async function handler(req, res) {
         id: "desc",
       },
     });
-
-    return res.status(200).json(jobs);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
   }
 }
+
+export default (req, res) => {
+  const page = Number(req.query.page) || 1; // クエリからページ番号を取得
+  const jobService = new JobService();
+
+  return jobService
+    .fetchJobs(page)
+    .then((jobs) => {
+      res.status(200).json(jobs);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
