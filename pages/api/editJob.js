897257e9e678de93e262e0430a553c23ae178a77
takehiro <iwasakitakehiro2@gmail.com>
2023-09-23 23:19:58 +0900

    デプロイように修正

--- pages/api/editJob.js
+++ pages/api/editJob.js
@@ -1,77 +1,59 @@
 import { prisma } from "@/lib/prisma";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const {
-        jobId,
-        imageUrl,
-        industry,
-        title,
-        description,
-        region,
-        location,
-        salary,
-        type,
-        location_detail,
-        working_hours_detail,
-        start_time,
-        finish_time,
-        salary_detail,
-        welfare,
-        vacation,
-      } = req.body;
-      if (imageUrl) {
-        const newJob = await prisma.job.update({
-          where: {
-            id: Number(jobId),
-          },
-          data: {
-            title: title,
-            type: type,
-            industry: industry,
-            location_detail: location_detail,
-            start_time: start_time,
-            imageUrl: imageUrl,
-            finish_time: finish_time,
-            salary_detail: salary_detail,
-            description: description,
-            working_hours_detail: working_hours_detail,
-            welfare: welfare,
-            location: location,
-            region: region,
-            vacation: vacation,
-            salary: salary,
-          },
-        });
-      } else {
-        const newJob = await prisma.job.update({
-          where: {
-            id: Number(jobId),
-          },
-          data: {
-            title: title,
-            type: type,
-            industry: industry,
-            location_detail: location_detail,
-            start_time: start_time,
-            finish_time: finish_time,
-            salary_detail: salary_detail,
-            description: description,
-            working_hours_detail: working_hours_detail,
-            welfare: welfare,
-            location: location,
-            region: region,
-            vacation: vacation,
-            salary: salary,
-          },
-        });
-      }
-      return res.status(201).json({ message: "求人情報を修正しました" });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: error.message });
-    }
+  if (req.method !== "POST") {
+    return res.status(405).end(); // Method Not Allowed
   }
-  res.status(405).end(); // Method Not Allowed
+
+  try {
+    const {
+      jobId,
+      imageUrl,
+      industry,
+      title,
+      description,
+      region,
+      location,
+      salary,
+      type,
+      location_detail,
+      working_hours_detail,
+      start_time,
+      finish_time,
+      salary_detail,
+      welfare,
+      vacation,
+    } = req.body;
+
+    const updateData = {
+      title,
+      type,
+      industry,
+      location_detail,
+      start_time,
+      finish_time,
+      salary_detail,
+      description,
+      working_hours_detail,
+      welfare,
+      location,
+      region,
+      vacation,
+      salary,
+    };
+
+    if (imageUrl) {
+      updateData.imageUrl = imageUrl;
+    }
+
+    await prisma.job.update({
+      where: { id: Number(jobId) },
+      data: updateData,
+    });
+
+    return res.status(201).json({ message: "求人情報を修正しました" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
 }
--- pages/api/getJobList.js
+++ pages/api/getJobList.js
@@ -1,43 +1,21 @@
 import { prisma } from "@/lib/prisma";
-import fs from "fs/promises";
-import path from "path";
 
 export default async function handler(req, res) {
   try {
+    const page = Number(req.query.page) || 1; // クエリからページ番号を取得
+    const limit = 20; // 1ページあたりのレコード数
+    const skip = (page - 1) * limit; // スキップするレコード数
+
     const jobs = await prisma.job.findMany({
+      skip: skip, // ページネーションのためのスキップ
+      take: limit, // 1ページあたりのレコード数
       include: {
-        company: true, // Include the related Company records
+        company: true,
       },
       orderBy: {
         id: "desc",
       },
     });
-
-    // ルートディレクトリのパスを取得
-    const rootDirectory = process.cwd();
-
-    // job.jsonファイルのフルパスを取得
-    const filePath = path.join(rootDirectory, "json", "job.json");
-
-    try {
-      // job.jsonファイルの最終変更日時を取得
-      const stat = await fs.stat(filePath);
-      const now = new Date();
-      const lastModified = new Date(stat.mtime);
-
-      // 最終変更日時から24時間以上経過しているか確認
-      if (now.getTime() - lastModified.getTime() > 24 * 60 * 60 * 1000) {
-        // 24時間以上経過している場合、ファイルを上書き
-        await fs.writeFile(filePath, JSON.stringify(jobs, null, 2), "utf-8");
-      }
-    } catch (error) {
-      // job.jsonが存在しない場合、新規にファイルを作成
-      if (error.code === "ENOENT") {
-        await fs.writeFile(filePath, JSON.stringify(jobs, null, 2), "utf-8");
-      } else {
-        throw error;
-      }
-    }
 
     return res.status(200).json(jobs);
   } catch (error) {
