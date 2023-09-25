3d16a116e4d9458b106314737581a0727b289696
takehiro <iwasakitakehiro2@gmail.com>
2023-09-25 12:36:46 +0900

    デプロイ

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -2,21 +2,16 @@ import CardCompornent from "@/components/card";
 
 export const getServerSideProps = async () => {
   try {
-    const responce = await fetch(`${process.env.DOMAIN}/api/getJobList`);
-    const data = await responce.json();
-    // // ルートディレクトリのパスを取得
-    // const rootDirectory = process.cwd();
-
-    // const filePath = path.join(rootDirectory, "json", "job.json");
-
-    // // JSONファイルを読み込む
-    // const rawData = await fs.readFile(filePath, "utf-8");
-
-    // // JSONデータを解析
-    // const data = JSON.parse(rawData);
+    const response = await fetch(`${process.env.DOMAIN}/api/getJobList`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
 
     return { props: { data } };
   } catch (error) {
+    // エラーログを出力することで、エラーの原因を特定しやすくします。
+    console.error("Error fetching job list:", error);
     return { notFound: true };
   }
 };
